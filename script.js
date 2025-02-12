dc6aae0a75faf3ea127b0dbe902a44a3842ca8a9
Yashtyagi1504 <yashtyagi1552@gmail.com>
2025-02-13 03:56:18 +0530

    mssg

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+let divs = document.querySelectorAll(".image");
+let count = 1;
+divs.forEach(div=>{
+    div.id = "div" + count;
+    count++;
+    div.addEventListener("dragstart",(e)=>{
+        e.dataTransfer.setData("text",div.id);
+    })
+});
+let arr = ["dragover","dragenter","drop"];
+
+arr.forEach(event =>{
+    body.addEventListener(event,(e)=>{
+        e.preventDefault();
+        if(event == "drop"){
+            let movingImage = document.querySelector("#" + e.dataTransfer.getData("text"));
+            body.append(movingImage)
+        }
+    })
+})
